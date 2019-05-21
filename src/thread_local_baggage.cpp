eddc52e3f489b9de7a0dce3dc10e43f937bc6203
Jonathan Mace <jcmace@mpi-sws.org>
2019-05-21 16:29:39 +0000

    Capitalize operation logging

--- src/thread_local_baggage.cpp
+++ src/thread_local_baggage.cpp
@@ -9,41 +9,41 @@ Baggage& ThreadLocalBaggage::Get() {
 }
 
 Baggage ThreadLocalBaggage::Take() {
-	XTRACE("ThreadLocalBaggage::Take", {{"operation", "unset"}});
+	XTRACE("ThreadLocalBaggage::Take", {{"Operation", "unset"}});
 	Baggage b = current_threads_baggage;
 	current_threads_baggage = Baggage();
 	return b;
 }
 
 Baggage ThreadLocalBaggage::Branch() {
-	XTRACE("ThreadLocalBaggage::Branch", {{"operation", "fork"}});
+	XTRACE("ThreadLocalBaggage::Branch", {{"Operation", "fork"}});
 	return current_threads_baggage;
 }
 
 void ThreadLocalBaggage::Join(Baggage &otherBaggage) {
 	current_threads_baggage = Baggage::merge(current_threads_baggage, otherBaggage);
-	XTRACE("ThreadLocalBaggage::Join", {{"operation", "fork"}});
+	XTRACE("ThreadLocalBaggage::Join", {{"Operation", "fork"}});
 }
 
 void ThreadLocalBaggage::Set(Baggage new_baggage) {
 	if (current_threads_baggage.atoms.size() == 0) {
 		current_threads_baggage = new_baggage; 
-		XTRACE("ThreadLocalBaggage::Set", {{"operation", "set"}});
+		XTRACE("ThreadLocalBaggage::Set", {{"Operation", "set"}});
 	} else {
 		current_threads_baggage = new_baggage; 		
 	}
 }
 
 Baggage ThreadLocalBaggage::Swap(Baggage other_baggage) {
-	XTRACE("ThreadLocalBaggage::Swap", {{"operation", "unset"}});
+	XTRACE("ThreadLocalBaggage::Swap", {{"Operation", "unset"}});
 	Baggage prev = current_threads_baggage;
 	current_threads_baggage = other_baggage;
-	XTRACE("ThreadLocalBaggage::Swap", {{"operation", "set"}});
+	XTRACE("ThreadLocalBaggage::Swap", {{"Operation", "set"}});
 	return prev;
 }
 
 void ThreadLocalBaggage::Delete() {
-	XTRACE("ThreadLocalBaggage::Delete", {{"operation", "unset"}});
+	XTRACE("ThreadLocalBaggage::Delete", {{"Operation", "unset"}});
 	current_threads_baggage = Baggage();	
 }
 
@@ -52,40 +52,40 @@ Baggage& ThreadLocalBaggage::Get(const char* file, int line) {
 }
 
 Baggage ThreadLocalBaggage::Take(const char* file, int line) {
-	XTRACE2(file, line, "ThreadLocalBaggage::Take", {{"operation", "unset"}});
+	XTRACE2(file, line, "ThreadLocalBaggage::Take", {{"Operation", "unset"}});
 	Baggage b = current_threads_baggage;
 	current_threads_baggage = Baggage();
 	return b;
 }
 
 Baggage ThreadLocalBaggage::Branch(const char* file, int line) {
-	XTRACE2(file, line, "ThreadLocalBaggage::Branch", {{"operation", "fork"}});
+	XTRACE2(file, line, "ThreadLocalBaggage::Branch", {{"Operation", "fork"}});
 	return current_threads_baggage;
 }
 
 void ThreadLocalBaggage::Join(Baggage &otherBaggage, const char* file, int line) {
 	current_threads_baggage = Baggage::merge(current_threads_baggage, otherBaggage);
-	XTRACE2(file, line, "ThreadLocalBaggage::Join", {{"operation", "fork"}});
+	XTRACE2(file, line, "ThreadLocalBaggage::Join", {{"Operation", "fork"}});
 }
 
 void ThreadLocalBaggage::Set(Baggage new_baggage, const char* file, int line) {
 	if (current_threads_baggage.atoms.size() == 0) {
 		current_threads_baggage = new_baggage; 
-		XTRACE2(file, line, "ThreadLocalBaggage::Set", {{"operation", "set"}});
+		XTRACE2(file, line, "ThreadLocalBaggage::Set", {{"Operation", "set"}});
 	} else {
 		current_threads_baggage = new_baggage; 		
 	}
 }
 
 Baggage ThreadLocalBaggage::Swap(Baggage other_baggage, const char* file, int line) {
-	XTRACE2(file, line, "ThreadLocalBaggage::Swap", {{"operation", "unset"}});
+	XTRACE2(file, line, "ThreadLocalBaggage::Swap", {{"Operation", "unset"}});
 	Baggage prev = current_threads_baggage;
 	current_threads_baggage = other_baggage;
-	XTRACE2(file, line, "ThreadLocalBaggage::Swap", {{"operation", "set"}});
+	XTRACE2(file, line, "ThreadLocalBaggage::Swap", {{"Operation", "set"}});
 	return prev;
 }
 
 void ThreadLocalBaggage::Delete(const char* file, int line) {
-	XTRACE2(file, line, "ThreadLocalBaggage::Delete", {{"operation", "unset"}});
+	XTRACE2(file, line, "ThreadLocalBaggage::Delete", {{"Operation", "unset"}});
 	current_threads_baggage = Baggage();	
 }
